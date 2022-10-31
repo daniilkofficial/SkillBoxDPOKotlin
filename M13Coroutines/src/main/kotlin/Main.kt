1fd9aae0bb894a413ad4f74a9c5b17f6644244f6
DaniilK <daniilkamornicki@gmail.com>
2022-10-31 22:57:44 +0300

    Task M13 Coroutines

--- M13Coroutines/src/main/kotlin/Main.kt
+++ M13Coroutines/src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>) {
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch{
+        delay(2000)
+        println("world !")
+    }
+
+    launch{
+        for(i in 1..500){
+            delay(10)
+            if(i % 130 == 0)
+                println(".")
+            else
+                println(".")
+        }
+    }
     println("Hello World!")
 }
