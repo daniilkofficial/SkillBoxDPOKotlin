48aa60acd5f88d913140e7156ba3a2b2c31cdeb4
DaniilK <daniilkamornicki@gmail.com>
2023-02-10 18:28:00 +0300

    Task 4 - Done. Task 5 - 9 work...

--- M13Coroutines/src/main/kotlin/Main.kt
+++ M13Coroutines/src/main/kotlin/Main.kt
@@ -13,13 +13,6 @@ fun main() {
         launch { fibonacci.take(10) }
         launch { fibonacci.take(15) }
     }
-
-//    var n = readlnOrNull()?.toIntOrNull() ?: -1
-//    while (n <= -1) {
-//        println("Ошибка: Введите целое число больше 0\n")
-//        n = readlnOrNull()?.toIntOrNull() ?: -1
-//        println(n)
-//    }
 }
 
 
