7ab5776ac1025ddac599cb10823ff247b9c84df5
DaniilK <daniilkamornicki@gmail.com>
2023-02-11 15:35:55 +0300

    Add yield and println Launch

--- M13Coroutines/src/main/kotlin/Main.kt
+++ M13Coroutines/src/main/kotlin/Main.kt
@@ -1,18 +1,28 @@
 import kotlinx.coroutines.*
 
 fun main() {
-    // до 7го задания
     val fibonacci = Fibonacci()
     runBlocking {
         withTimeout(1000) {
             launch {
+                println("Launch 1")
                 fibonacci.take(5)
-                currentCoroutineContext().isActive
+//                currentCoroutineContext().isActive
             }
         }
 
-        launch { fibonacci.take(10) }
-        launch { fibonacci.take(15) }
+        val job2 = launch {
+            println("Launch 2")
+            fibonacci.take(10)
+        }
+        yield()
+        job2.cancel()
+        yield()
+
+        launch {
+            println("Launch 3")
+            fibonacci.take(15)
+        }
     }
 }
 
