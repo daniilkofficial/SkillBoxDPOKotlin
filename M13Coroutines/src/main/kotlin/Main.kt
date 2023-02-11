53452ddd728008a24bca1b950842deaaa4afde34
DaniilK <daniilkamornicki@gmail.com>
2023-02-11 15:29:25 +0300

    Add withTimeout and currentCoroutineContext

--- M13Coroutines/src/main/kotlin/Main.kt
+++ M13Coroutines/src/main/kotlin/Main.kt
@@ -4,14 +4,13 @@ fun main() {
     // до 7го задания
     val fibonacci = Fibonacci()
     runBlocking {
-        val job1 = launch {
-            fibonacci.take(5)
-            currentCoroutineContext().isActive
-            // yield()
+        withTimeout(1000) {
+            launch {
+                fibonacci.take(5)
+                currentCoroutineContext().isActive
+            }
         }
-        withTimeout(3000) {
-            job1.cancel()
-        }
+
         launch { fibonacci.take(10) }
         launch { fibonacci.take(15) }
     }
