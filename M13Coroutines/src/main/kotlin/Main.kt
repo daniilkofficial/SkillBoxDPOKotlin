e7198647cf9445ce7a0f10aab006d84fa38c9189
DaniilK <daniilkamornicki@gmail.com>
2023-02-11 16:31:58 +0300

    Done work

--- M13Coroutines/src/main/kotlin/Main.kt
+++ M13Coroutines/src/main/kotlin/Main.kt
@@ -1,33 +1,40 @@
 import kotlinx.coroutines.*
 
+val scope = CoroutineScope(Job() + Dispatchers.Default)
 
 fun main() {
     val fibonacci = Fibonacci()
     runBlocking {
         try {
-            withTimeout(1000) {
-                launch {
+            scope.launch {
+                withTimeout(1000) {
                     println("Launch 1")
-                    fibonacci.take(100)
-//                currentCoroutineContext().isActive
+                    fibonacci.take(120)
                 }
             }
         } catch (e: TimeoutCancellationException) {
             println("Превышено время вычисления")
         }
 
-        val job2 = launch {
+        val job2 = scope.launch {
             println("Launch 2")
             fibonacci.take(10)
         }
+        job2.join()
         yield()
         job2.cancel()
         yield()
 
-        launch {
+        scope.launch {
             println("Launch 3")
-            fibonacci.take(15)
+            fibonacci.take(35)
         }
+
+        scope.launch {
+            while (true)
+                print(".")
+        }
+
     }
 }
 
