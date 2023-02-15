8c05f3078eb8adf578e1beb745c8d9bad1b6f0be
DaniilK <daniilkamornicki@gmail.com>
2023-02-15 09:23:15 +0300

    Fix and done M14Flow

--- M14Flow/src/main/kotlin/Main.kt
+++ M14Flow/src/main/kotlin/Main.kt
@@ -11,82 +11,44 @@ fun main() {
 
     println("Внимание! Игра начинается")
     var isWin = false
+    val players = arrayOf(1, 2, 3)
 
     runBlocking {
-        launch {
-            val listBank1: ArrayList<Int> = getGameCard()
-            println("Игрок 1 начал игру")
-            Generator.flow1.collect {
-                println("Игрок 1. Бочонок $it. Осталось найти: ${listBank1.size}")
-                listBank1.remove(it)
+        players.forEach { num ->
+            val listBank: ArrayList<Int> = getGameCard()
+            println("Игрок $num начал игру")
 
-                if (isWin) {
-                    cancel()
-                } else if (listBank1.size == 0) {
-                    println("Победил игрок 1!")
-                    isWin = true
-                    cancel()
-                }
+            launch {
+                Generator.flow.collect {
+                    println("Игрок $num. Бочонок $it. Осталось найти: ${listBank.size}")
+                    listBank.remove(it)
 
-            }
-        }
-        launch {
-            val listBank2: ArrayList<Int> = getGameCard()
-            println("Игрок 2 начал игру")
-            Generator.flow2.collect {
-                println("Игрок 2. Бочонок $it. Осталось найти: ${listBank2.size}")
-                listBank2.remove(it)
+                    if (isWin) {
+                        cancel()
+                    } else if (listBank.size == 0) {
+                        println("Победил игрок $num!")
+                        isWin = true
+                        cancel()
+                    }
 
-                if (isWin) {
-                    cancel()
-                } else if (listBank2.size == 0) {
-                    println("Победил игрок 2!")
-                    isWin = true
-                    cancel()
                 }
             }
-        }
-        launch {
-            val listBank3: ArrayList<Int> = getGameCard()
-            println("Игрок 3 начал игру")
-            Generator.flow3.collect {
-                println("Игрок 3. Бочонок $it. Осталось найти: ${listBank3.size}")
-                listBank3.remove(it)
 
-                if (isWin) {
-                    cancel()
-                } else if (listBank3.size == 0) {
-                    println("Победил игрок 3!")
-                    isWin = true
-                    cancel()
-                }
-            }
         }
+
     }
+
 }
 
 fun getGameCard(): ArrayList<Int> {
-    return arrayListOf(6, 8, 14, 20, 28, 39, 41, 46, 55, 62, 65, 70, 79, 82, 90)  // 15
+    return arrayListOf(6, 8, 14, 20, 28, 39, 41, 46, 55, 62, 65, 70, 79, 82, 90)  // 15 шт
 }
 
 object Generator {
-    val flow1 = flow {
+    val flow = flow {
         while (currentCoroutineContext().isActive) {
             emit(Random.nextInt(1, 91))
-            delay(400)
-        }
-    }
-
-    val flow2 = flow {
-        while (currentCoroutineContext().isActive) {
-            emit(Random.nextInt(1, 91))
-            delay(300)
-        }
-    }
-    val flow3 = flow {
-        while (currentCoroutineContext().isActive) {
-            emit(Random.nextInt(1, 91))
-            delay(100)
+            yield()
         }
     }
 }
